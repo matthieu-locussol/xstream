384efb172d4878ce9f617412e9cfb2b1546d3150
Matthieu Locussol <matthieu@galadrim.fr>
2021-02-25 21:44:56 +0100

    feat: updated repository informations

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,6 @@
-import Link from 'next/link';
-
 const Index = (): JSX.Element => (
    <div>
       <h1>Hello world!</h1>
-      <p>You are using Matthieu LOCUSSOL's Tauri starter using Typescript and Next.js!</p>
-      <Link href="/posts">Show blog posts</Link>
    </div>
 );
 
