abfcf4d0bc22ea914eb481018633234801fdc332
Matthieu Locussol <matthieu@galadrim.fr>
2021-02-28 15:20:01 +0100

    feat: added Next _page and _document components

---
+++ src/pages/_app.tsx
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react';
+import Head from 'next/head';
+import type { AppProps } from 'next/app';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { themeDark } from '@/themes/dark';
+
+export default function MyApp(props: AppProps): JSX.Element {
+   const { Component, pageProps } = props;
+
+   useEffect(() => {
+      const jssStyles = document.querySelector('#jss-server-side');
+      jssStyles?.parentElement?.removeChild(jssStyles);
+   }, []);
+
+   return (
+      <ThemeProvider theme={themeDark}>
+         <Head>
+            <title>XStream ・ Modern playlist manager</title>
+         </Head>
+         <CssBaseline />
+         <Component {...pageProps} />
+      </ThemeProvider>
+   );
+}
